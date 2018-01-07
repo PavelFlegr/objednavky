d63a5198462e57b889be349197feaef05085ab5b
Pavel Flegr <pavelflegr@gmail.com>
2018-01-07 16:54:12 +0100

    z

--- testapi/Profile.xaml.cs
+++ testapi/Profile.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RestSharp;
 
 namespace testapi
 {
@@ -24,12 +25,25 @@ namespace testapi
         {
             InitializeComponent();
             name.Text = Global.username;
+            var response = Global.client.Execute<UserInfo>(new RestRequest("user", Method.GET));
+            ulice.Text = response.Data.ulice;
+            psc.Text = response.Data.psc;
+            cp.Text = response.Data.cp;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MainWindow.window.Login();
             MainWindow.window.navigation.Navigate(new Items());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var request = new RestRequest("user", Method.POST);
+            request.AddParameter("ulice", ulice.Text);
+            request.AddParameter("psc", psc.Text);
+            request.AddParameter("cp", cp.Text);
+            Global.client.Execute(request);
         }
     }
 }
---
+++ testapi/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testapi
+{
+    public class UserInfo
+    {
+        public string ulice { get; set; }
+        public string psc { get; set; }
+        public string cp { get; set; }
+    }
+}
